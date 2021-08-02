695433b19b6bc4c071f01ddef466fe1a52a381bc
Antonio Russo <russoantonio1997@gmail.com>
2021-08-02 09:36:22 +0200

    fix: axios types

--- src/utils/httpProvider.ts
+++ src/utils/httpProvider.ts
@@ -1,16 +1,8 @@
-import { env } from '../../env'
-import { JIRA_JWT_TOKEN } from '../JiraWrapper/index'
-import Axios, { AxiosInstance } from 'axios'
+import Axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
 
-const instance: AxiosInstance = Axios.create({
-  url: `${env.JIRA_API_URL}/${env.JIRA_API_VERSION}`,
-  baseURL: env.JIRA_API_BASE_URL,
-  headers: {
-    Authorization: JIRA_JWT_TOKEN
-  }
-})
+const instance: AxiosInstance = Axios.create({})
 
-export const httpPost = (url, data, config?) => instance.post(url, data, config)
-export const httpGet = (url, data?) => instance.get(url, data)
-export const httpUpdate = (url, data, config?) => instance.patch(url, data, config)
-export const httpDelete = (url, config?) => instance.delete(url, config)
+export const httpPost = (url: string, data?: any, config?: AxiosRequestConfig) => instance.post(url, data, config)
+export const httpGet = (url: string, data?: any) => instance.get(url, data)
+export const httpUpdate = (url: string, data: any, config?: AxiosRequestConfig) => instance.patch(url, data, config)
+export const httpDelete = (url: string, config?: AxiosRequestConfig) => instance.delete(url, config)
